192fb279069cbd4bb5954aa6feab126b3dcda07c
zulkarnine <zulkarnine2076@gmail.com>
2014-06-23 20:33:04 +0900

    added two comments

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test for repository");
 		//changed something
+		
+		// added another comment
+		
+		//another comment
 	}
 	
 	public static void testMethod() {
